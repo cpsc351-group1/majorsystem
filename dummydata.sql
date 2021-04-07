3f6378aa267f98d39234479844066b877bb4eb42
samtyler17 <samuel.tyler.17@cnu.edu>
2021-04-06 20:16:50 -0400

    add dummy data
    users and committees exclusively for now. 10 users, 5 committees.

--- dummydata.sql
+++ dummydata.sql
@@ -1,15 +1,48 @@
-INSERT INTO committee VALUES(
-'NAME OF COMMITTEE', 'DESCRIPTION');
+/*    USERS   */
 
-INSERT INTO committee seat VALUES(
-'DESCRIPTION', 'STARTING TERM', 'ENDING TERM', 'CHAIRMAN', 'USER CNU ID');
+INSERT IGNORE INTO user
+  VALUES (00998877, 'John Doe', 'Molecular Biology and Chemistry', 'Associate Professor', '1979-01-01', '2020-01-01', 'Male', 'White');
 
-INSERT INTO election VALUES(
-'STATUS', 'TERM ID');
+INSERT IGNORE INTO user
+  VALUES (00987987, 'Jill Doe', 'English', 'Professor', '1975-01-01', '2015-01-01', 'Female', 'Black');
 
-INSERT INTO committee VALUES(
-'NAME OF COMMITTEE', 'DESCRIPTION');
+INSERT IGNORE INTO user
+  VALUES (00978879, 'Beyoncé Knowles-Carter', 'Music', 'Department Lead', '1981-09-04', '2008-05-03', 'Female', 'Black');
 
-INSERT INTO user VALUES(
-'CNU ID', 'NAME', 'DEPARTMENT', 'POSITION', 'BIRTHDAY', 'DATE OF HIRING', 
-'GENDER', 'RACE');
+INSERT IGNORE INTO user
+  VALUES (00966678, 'Claire Boucher', 'Music', 'Associate Professor', '1988-03-17', '2018-01-04', 'Non-binary', 'White');
+
+INSERT IGNORE INTO user
+  VALUES (00942069, 'Elizabeth Grant', 'Philosophy and Religion', 'Professor', '1985-06-21', '2005-04-09', 'Female', 'White');
+
+INSERT IGNORE INTO user
+  VALUES (00933833, 'Azealia Banks', 'Communications', 'Adjunct', '1991-05-31', '2020-02-27', 'Female', 'Black');
+
+INSERT IGNORE INTO user
+  VALUES (00999919, 'Kurt Cobain', 'Economics', 'Associate Professor', '1967-02-20', '2018-12-20', 'Male', 'White');
+
+INSERT IGNORE INTO user
+  VALUES (00955259, 'Dolly Parton', 'Molecular Biology and Chemistry', 'Department Lead', '1946-01-19', '1998-04-30', 'Female', 'White');
+
+INSERT IGNORE INTO user
+  VALUES (00982429, 'Mike Lapke', 'Physics, Computer Science and Engineering', 'Associate Professor', '1960-02-03', '06-30-2020', 'Male', 'White');
+
+INSERT IGNORE INTO user
+  VALUES (09440044, 'Onika Maraj-Petty', 'Luter School of Business', 'Department Lead', '1982-12-08', '2000-09-15', 'Female', 'Black');
+
+/*    COMMITTEES    */
+
+INSERT IGNORE INTO committee (Name, Description)
+  VALUES ('Sustainability Committee', 'Committee that oversees sustainability.');
+
+INSERT IGNORE INTO committee (Name, Description)
+  VALUES ('Fun Committee', 'Committee that oversees fun activities.');
+
+INSERT IGNORE INTO committee (Name, Description)
+  VALUES ('Grass Committee', 'Committee that oversees the Great Lawn.');
+
+INSERT IGNORE INTO committee (Name, Description)
+  VALUES ('Money Committee', 'Committee that oversees financials.');
+
+INSERT IGNORE INTO committee (Name, Description)
+  VALUES ('University Faculty on Committees', 'Committee that oversees committees.');
