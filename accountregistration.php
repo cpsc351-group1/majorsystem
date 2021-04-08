178fd100a6660c71a01b556601e7db4baaa1a56b
mattorr67 <82040016+mattorr67@users.noreply.github.com>
2021-04-08 09:59:19 -0400

    Update accountregistration.php

--- accountregistration.php
+++ accountregistration.php
@@ -1,9 +1,34 @@
-<!DOCTYPE php>
 <html>
   <head>
+   <style>
+      body>* {
+        padding: 1em;
+      }
+      body {
+        width: 70%;
+        margin: 1em auto;
+        text-align: center;
+        color: #333333;
+      }
+      i {
+      font-weight: bold;
+      color: black;
+      }
+      form {
+        outline: 1px solid black;
+      }
+      p {
+        outline: 1px solid #DDDDDD;
+      }
+    </style>
     <title>Account Registration</title>
   </head>
   <body>
-    <?php echo '<h1>test</h1>' ?>
+    <?php 
+    echo "<html><body>
+	<form action=\"odis.php\" method=\"post\">
+	Click the submit button to view the Master List! 
+	<br><br><input type=\"submit\" name=\"submit\"><br><br>";
+	 ?>
   </body>
 </html>
