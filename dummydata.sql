99f1b7d178a321a1d9b6251b20597213b0a769d2
samtyler17 <samuel.tyler.17@cnu.edu>
2021-04-13 19:34:11 -0400

    dummy data fixes
    converted truncate commands (cannot work with foreign key contraints) to delete commands
    
    converted "INSERT IGNORE" commands into "INSERT" commands for clarity
    
    fixed syntactical errors (oops)
    
    added data for updated user database design (fname, lname, email)

--- dummydata.sql
+++ dummydata.sql
@@ -1,98 +1,98 @@
+DELETE FROM `chairman`;
+DELETE FROM `committee seat`;
+DELETE FROM `committee`;
+DELETE FROM `user`;
+
 /*    USERS   */
 
-TRUNCATE TABLE `committee seat`;
-TRUNCATE TABLE `chairman`;
-TRUNCATE TABLE `committee`;
-TRUNCATE TABLE `user`;
+INSERT INTO `user`
+  VALUES (00998877, 'John', 'Doe', 'johndoe@cnu.edu', 'Molecular Biology and Chemistry', 'Associate Professor', '1979-01-01', '2020-01-01', 'Male', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00998877, 'John Doe', 'Molecular Biology and Chemistry', 'Associate Professor', '1979-01-01', '2020-01-01', 'Male', 'White');
+INSERT INTO `user`
+  VALUES (00987987, 'Jill', 'Doe', 'jilldoe@cnu.edu', 'English', 'Professor', '1975-01-01', '2015-01-01', 'Female', 'Black', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00987987, 'Jill Doe', 'English', 'Professor', '1975-01-01', '2015-01-01', 'Female', 'Black');
+INSERT INTO `user`
+  VALUES (00978879, 'Beyoncé', 'Knowles-Carter', 'beyonceknowles@cnu.edu', 'Music', 'Department Lead', '1981-09-04', '2008-05-03', 'Female', 'Black', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00978879, 'Beyoncé Knowles-Carter', 'Music', 'Department Lead', '1981-09-04', '2008-05-03', 'Female', 'Black');
+INSERT INTO `user`
+  VALUES (00966678, 'Claire', 'Boucher', 'claireboucher@cnu.edu', 'Graphic Design', 'Associate Professor', '1988-03-17', '2018-01-04', 'Non-binary', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00966678, 'Claire Boucher', 'Graphic Design', 'Associate Professor', '1988-03-17', '2018-01-04', 'Non-binary', 'White');
+INSERT INTO `user`
+  VALUES (00942069, 'Elizabeth', 'Grant', 'elizabethgrant@cnu.edu', 'Philosophy and Religion', 'Professor', '1985-06-21', '2005-04-09', 'Female', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00942069, 'Elizabeth Grant', 'Philosophy and Religion', 'Professor', '1985-06-21', '2005-04-09', 'Female', 'White');
+INSERT INTO `user`
+  VALUES (00933833, 'Azealia', 'Banks', 'azealiabanks@cnu.edu', 'Communications', 'Adjunct', '1991-05-31', '2020-02-27', 'Female', 'Black', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00933833, 'Azealia Banks', 'Communications', 'Adjunct', '1991-05-31', '2020-02-27', 'Female', 'Black');
+INSERT INTO `user`
+  VALUES (00999919, 'Kurt', 'Cobain', 'kurtcobain@cnu.edu', 'Economics', 'Associate Professor', '1967-02-20', '2018-12-20', 'Male', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00999919, 'Kurt Cobain', 'Economics', 'Associate Professor', '1967-02-20', '2018-12-20', 'Male', 'White');
+INSERT INTO `user`
+  VALUES (00955259, 'Dolly', 'Parton', 'dollyparton@cnu.edu', 'Molecular Biology and Chemistry', 'Department Lead', '1946-01-19', '1998-04-30', 'Female', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00955259, 'Dolly Parton', 'Molecular Biology and Chemistry', 'Department Lead', '1946-01-19', '1998-04-30', 'Female', 'White');
+INSERT INTO `user`
+  VALUES (00982429, 'Mike', 'Lapke', 'mikelapke@cnu.edu', 'Physics, Computer Science and Engineering', 'Associate Professor', '1960-02-03', '2020-06-30', 'Male', 'White', NULL);
 
-INSERT IGNORE INTO user
-  VALUES (00982429, 'Mike Lapke', 'Physics, Computer Science and Engineering', 'Associate Professor', '1960-02-03', '06-30-2020', 'Male', 'White');
-
-INSERT IGNORE INTO user
-  VALUES (00944004, 'Onika Maraj-Petty', 'Luter School of Business', 'Department Lead', '1982-12-08', '2000-09-15', 'Female', 'Black');
+INSERT INTO `user`
+  VALUES (00944004, 'Onika', 'Maraj-Petty', 'onikamaraj@cnu.edu', 'Luter School of Business', 'Department Lead', '1982-12-08', '2000-09-15', 'Female', 'Black', NULL);
 
 /*    COMMITTEES    */
 
-INSERT IGNORE INTO committee (Name, Description)
+INSERT INTO `committee` (Name, Description)
   VALUES ('Sustainability Committee', 'Committee that oversees sustainability.');
 
-INSERT IGNORE INTO committee (Name, Description)
+INSERT INTO `committee` (Name, Description)
   VALUES ('Fun Committee', 'Committee that oversees fun activities.');
 
-INSERT IGNORE INTO committee (Name, Description)
+INSERT INTO `committee` (Name, Description)
   VALUES ('Grass Committee', 'Committee that oversees the Great Lawn.');
 
-INSERT IGNORE INTO committee (Name, Description)
+INSERT INTO `committee` (Name, Description)
   VALUES ('Money Committee', 'Committee that oversees financials.');
 
-INSERT IGNORE INTO committee (Name, Description)
+INSERT INTO `committee` (Name, Description)
   VALUES ('University Faculty on Committees', 'Committee that oversees committees.');
 
-/*  COMMITTEE SEATS   */
+/*  `committee seat`S   */
+/* TODO: give some of these end_date values for testing archived seats */
 
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (1, 'Fall 2019', NULL, 00998877);
 
-INSERT IGNORE INTO committee seat
-  VALUES (1, 1, 'Fall 2019', NULL, 00998877);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (1, 'Spring 2020', NULL, 00987987);
 
-INSERT IGNORE INTO committee seat
-  VALUES (2, 1, 'Spring 2020', NULL, 00987987);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (1, 'Fall 2020', NULL, 00978879);
 
-INSERT IGNORE INTO committee seat
-  VALUES (3, 1, 'Fall 2020', NULL, 00978879);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (2, 'Spring 2021', NULL, 00966678);
 
-INSERT IGNORE INTO committee seat
-  VALUES (4, 2, 'Spring 2021', NULL, 00966678);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (2, 'Fall 2020', NULL, 00942069);
 
-INSERT IGNORE INTO committee seat
-  VALUES (5, 2, 'Fall 2020', NULL, 00942069);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (3, 'Spring 2021', NULL, 00933833);
 
-INSERT IGNORE INTO committee seat
-  VALUES (6, 3, 'Spring 2021', NULL, 00933833);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (3, 'Fall 2019', NULL, 00999919);
 
-INSERT IGNORE INTO committee seat
-  VALUES (7, 3, 'Fall 2019', NULL, 00999919);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (4, 'Spring 2021', NULL, 00955259);
 
-INSERT IGNORE INTO committee seat
-  VALUES (8, 4, 'Spring 2021', NULL, 00955259);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (5, 'Spring 2020', NULL, 00982429);
 
-INSERT IGNORE INTO committee seat
-  VALUES (9, 5, 'Spring 2020', NULL, 00982429);
-
-INSERT IGNORE INTO committee seat
-  VALUES (10, 5, 'Fall 2020', NULL, 00944004);
+INSERT INTO `committee seat` (Committee_Committee_ID, Starting_Term, Ending_Term, User_CNU_ID)
+  VALUES (5, 'Fall 2020', NULL, 00944004);
 
 /*    CHAIRMAN    */
 
-INSERT IGNORE INTO chairman VALUES (1, 00998877);
+INSERT INTO chairman VALUES (1, 00998877);
 
-INSERT IGNORE INTO chairman VALUES (2, 00966678);
+INSERT INTO chairman VALUES (2, 00966678);
 
-INSERT IGNORE INTO chairman VALUES (3, 00933833);
+INSERT INTO chairman VALUES (3, 00933833);
 
-INSERT IGNORE INTO chairman VALUES (4, 00955259);
+INSERT INTO chairman VALUES (4, 00955259);
 
-INSERT IGNORE INTO chairman VALUES (5, 00982429);
+INSERT INTO chairman VALUES (5, 00982429);
