3ab24eb09b51548e3ef465c7d27ead27c65218f0
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2021-04-15 09:40:00 -0400

    added image uploading capabilities to account_registration

--- account_registration.php
+++ account_registration.php
@@ -68,6 +68,11 @@
           <div class="column">
             <div class="block submit">
               <!-- TODO: add photo uploading capability -->
+              <form action="/action_page.php">
+                <label for="img">Select image:</label>
+                <input type="file" id="img" name="img" accept="image/*">
+                <input type="submit">
+              </form>
               <input id='submit' type="submit" value="Create Account">
             </div>
           </div>
