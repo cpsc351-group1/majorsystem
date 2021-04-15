d4f644c78ccc39646a09354190d36d79ac4088c9
mattorr67 <82040016+mattorr67@users.noreply.github.com>
2021-04-15 10:13:17 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,20 +1,9 @@
-<?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-?>
- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   <meta charset="utf-8">
+    <link rel="stylesheet" href="css/selection.css">
+    <?php include 'databaseconnect.php'; ?>
     <style>
     
         body{ font: 14px sans-serif; text-align: center; 
@@ -29,7 +18,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <?php
   
 	echo "<html><body>
-	<form action=\"HomeScreen.php\" method=\"post\">
+	<form action=\"welcome.php\" method=\"post\">
 	Click the submit button to view the Master List! 
 	<br><br><input type=\"submit\" name=\"submit\"><br><br>";
 	
