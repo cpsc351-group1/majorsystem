6940dfdc58daf4bda0189cfe1ff648d10913a32f
samtyler17 <samuel.tyler.17@cnu.edu>
2021-04-18 20:54:31 -0400

    forced redirect to index if no session set
    requires login to access any page besides index.php

--- databaseconnect.php
+++ databaseconnect.php
@@ -15,6 +15,10 @@ if ($conn->connect_error) {
     die("Oh no! Connection failed: " . $conn->connect_error);
 }
 
+if (basename($_SERVER['SCRIPT_FILENAME']) != "index.php" and is_null($_SESSION['user'])) {
+  header("Location: index.php");
+}
+
 // Boolean to track if already in database
 // $found = FALSE:
 
