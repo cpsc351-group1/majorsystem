22b8b7d0ead37ae808be8fc56faa624a21edb728
samtyler17 <samuel.tyler.17@cnu.edu>
2021-04-19 01:15:36 -0400

    user profile touchup
    changed layout and added spot for a photo, need to add committee memberships ...

--- user_details.php
+++ user_details.php
@@ -29,9 +29,10 @@
 
     ?>
 
-    <title>CNU — <?php echo $user['Fname']." ".$user['Lname']; ?></title>
-  </head>
-  <body>
+  <title>CNU — <?php echo $user['Fname']." ".$user['Lname']; ?></title>
+</head>
+
+<body>
 
   <!-- TODO: Create PHP script to generate this page for all
                users in a report    -->
@@ -40,53 +41,67 @@
     <header>
       <h2>User Details</h2>
     </header>
-    <div class="profile">
+    <div class="body">
+      <div class="column">
+        <!-- Heading -->
+        <span class="major heading"><?php echo $user['Fname'].' '.$user['Lname'];?></span>
+        <hr>
+        <div class="profile">
 
-      <!-- TODO: Add user photo and committee memberships -->
+          <!-- TODO: Add user photo and committee memberships -->
+            <div class="wrap_profile">
+              <div class="block">
+                <span class="sub heading">Personal Info</span>
 
-      <div class="body">
-        <div class="column">
-          <div class="block">
-            <span class="sub heading">Personal Info</span>
-            <span class="label">First Name:</span>
-            <span><?php echo $user['Fname'];?></span>
+                <span class="label">Email:</span>
+                <span><?php echo $user['Email'];?></span>
 
-            <span class="label">Last Name:</span>
-            <span><?php echo $user['Lname'];?></span>
+                <span class="label">Birthday:</span>
+                <span><?php echo $user['Birthday'];?></span>
+              </div>
 
-            <span class="label">Email:</span>
-            <span><?php echo $user['Email'];?></span>
+              <div class="block">
+                <span class="sub heading">Other</span>
+                <span class="label">Race:</span>
+                <span><?php echo $user['Race'];?></span>
 
-            <span class="label">Birthday:</span>
-            <span><?php echo $user['Birthday'];?></span>
-          </div>
+                <span class="label">Gender:</span>
+                <span><?php echo $user['Gender'];?></span>
+              </div>
+
+              <div class="block">
+                <span class="sub heading">Employment</span>
+                <span class="label">College:</span>
+                <span><?php echo $user['Department'];?></span>
+
+                <span class="label">Position:</span>
+                <span><?php echo $user['Position'];?></span>
+
+                <span class="label">Date of Hiring:</span>
+                <span><?php echo $user['Date_of_Hiring'];?></span>
+              </div>
+            </div>
+            <div class="right">
+              <div class="image">
+                <!-- TODO: dynamically insert image -->
+              </div>
+              <?php
+                $user_id = $user['CNU_ID'];
+                echo "<form action='user_details_update.php' method='get'><button name='user' value='$user_id'>Modify Profile</button></form>";
+              ?>
+            </div>
+
         </div>
-        <div class="column">
-          <div class="block">
-            <span class="sub heading">Employment</span>
-            <span class="label">College:</span>
-            <span><?php echo $user['Department'];?></span>
-
-            <span class="label">Position:</span>
-            <span><?php echo $user['Position'];?></span>
-
-            <span class="label">Date of Hiring:</span>
-            <span><?php echo $user['Date_of_Hiring'];?></span>
-          </div>
-        </div>
-        <div class="column">
-          <div class="block">
-            <span class="sub heading">Other</span>
-            <span class="label">Race:</span>
-            <span><?php echo $user['Race'];?></span>
-
-            <span class="label">Gender:</span>
-            <span><?php echo $user['Gender'];?></span>
-          </div>
+        <hr>
+        <span class="heading major">Committee Membership</span>
+        <div class="profile">
+          
         </div>
       </div>
+
     </div>
   </div>
+
   <?php $conn->close(); ?>
 </body>
 
