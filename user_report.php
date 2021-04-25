151923e6bd1e860192cb39818734a715dde1b0c8
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2021-04-25 14:08:24 -0400

    finished work on generating multiple reports

--- user_report.php
+++ user_report.php
@@ -27,13 +27,55 @@
     <?php
       while ($row = $id_list->fetch_assoc()) {
         $id = $row['CNU_ID'];
-      
+        $id_data_request = "SELECT * FROM User WHERE CNU_ID = $id";
+        $user = $conn->query($id_data_request)->fetch_assoc();     
 
         if (isset($_POST[$id])) {
-          echo "<div class=\"body\">";
-            echo "<div class=\"column\">";
-              echo "details for user $id";
-            echo "</div></div>";
+?>
+          <div class="body">
+            <div class="column">
+          <div class="tiles">
+            <div class="tile">
+              <span class="sub heading">Personal Info</span>
+              <hr>
+              <div class="list">
+                <span class="label">Email:</span>
+                <span><?php echo $user['Email'];?></span>
+
+                <span class="label">Birthday:</span>
+                <span><?php echo $user['Birthday'];?></span>
+              </div>
+            </div>
+
+            <div class="tile">
+              <span class="sub heading">Other</span>
+              <hr>
+              <div class="list">
+                <span class="label">Race:</span>
+                <span><?php echo $user['Race'];?></span>
+
+                <span class="label">Gender:</span>
+                <span><?php echo $user['Gender'];?></span>
+              </div>
+            </div>
+
+            <div class="tile">
+              <span class="sub heading">Employment</span>
+              <hr>
+              <div class="list">
+                <span class="label">Department:</span>
+                <span><?php echo $user['Department'];?></span>
+
+                <span class="label">Position:</span>
+                <span><?php echo $user['Position'];?></span>
+
+                <span class="label">Year of Hiring:</span>
+                <span><?php echo $user['Hiring_Year'];?></span>
+              </div>
+            </div>
+          </div>
+            </div></div>
+<?php
         }
       }
     ?>
