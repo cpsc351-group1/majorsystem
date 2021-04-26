f71ed06faca308724217c3ea358678eaf8eda674
samtyler17 <samuel.tyler.17@cnu.edu>
2021-04-26 17:54:29 -0400

    select/deselect all

--- js/selection_menu.js
+++ js/selection_menu.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
     // grey out report button automatically
-   $('#report').prop('disabled', true);
+  $('#report').prop('disabled', true);
 
-   // report button only greyed out when no boxes checked
-   $('.checkbox').click(function() {
-     if ($(this).is(":checked")) {
-       $('#report').prop('disabled', false);
-     } else if ($('.checkbox').filter(':checked').length == 0) {
-       $('#report').prop('disabled', true);
-     }
-   });
+  // report button only greyed out when no boxes checked
+  $('.checkbox').click(function() {
+    if ($(this).is(":checked")) {
+      $('#report').prop('disabled', false);
+    } else if ($('.checkbox').filter(':checked').length == 0) {
+      $('#report').prop('disabled', true);
+    }
+  });
 
-   $('#select_all').click(function() {
-    $('.checkbox').prop('checked', true);
-   });
+  $('#select_all').click(function() {
+  $('.checkbox').prop('checked', true);
+  });
 
-   $('#deselect_all').click(function() {
-    $('.checkbox').prop('checked', false);
-   });
+  $('#deselect_all').click(function() {
+  $('.checkbox').prop('checked', false);
+  });
+
+  $(document).on("input", function() {
+    update_search($("search"), $(".results"));
+  });
  });
